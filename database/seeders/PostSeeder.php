69e39fe3ac38b6f7b06abe52e9983ea95dea0b11
durlavk98 <durlavk98@gmail.com>
2021-05-31 17:20:52 +0530

    dummy data in laravel

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::create([
+            'title' => 'lorem ipsum dolor imit',
+            'body' => 'Similique molestias exercitationem officia aut. Itaque doloribus et rerum voluptate iure. Unde veniam magni dignissimos expedita eius.',
+        ]);
+    }
+}
